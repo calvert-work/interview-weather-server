49e84a55e1b7776482029a5bd83e711c18a04658
Calvert <wongcalvert-work@outlook.com>
2025-06-13 00:12:55 -0700

    Added invalid city check to current weather by city controller, added forecast by city controller, moved user input city value check to a middleware, moved open weather api details check to a middleware

---
+++ middlewares/hasCityValueCheck.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+
+export const hasCityValueCheck = (req: Request, res: Response, next: NextFunction) => {
+	const city = req.params.city;
+
+	if (!city) {
+		res.status(400).json({
+			message: "Invalid user input: city is not provided"
+		});
+		return;
+	}
+
+	next();
+};
---
+++ middlewares/openWeatherApiDetailsCheck.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+
+export const openWeatherApiDetailsCheck = (_: Request, res: Response, next: NextFunction) => {
+	const weatherApiKey = process.env.OPEN_WEATHER_API_KEY;
+	const weatherUrl = process.env.OPEN_WEATHER_URL;
+
+	if (!weatherApiKey) {
+		res.status(500).json({
+			message: "Invalid weather api key"
+		});
+		return;
+	}
+
+	if (!weatherUrl) {
+		res.status(500).json({
+			message: "Invalid weather url"
+		});
+		return;
+	}
+
+	next();
+};
