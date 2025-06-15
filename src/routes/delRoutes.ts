c77ef1e96c9386d562cb031e370463a8e1903385
Calvert <wongcalvert-work@outlook.com>
2025-06-15 02:59:51 -0700

    Added delete search history by user id controller and its delete route

--- src/routes/delRoutes.ts
+++ src/routes/delRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { deleteFavoriteCity } from "../controllers/favoriteCity/deleteFavoriteCity";
+import { headerHasUserIdCheck } from "../middlewares/validators/headerHasUserIdCheck";
+import { deleteSearchHistory } from "../controllers/searchHistory/deleteSearchHistory";
 
 export const delRouter = Router();
 
 // added this route to handle the situation where path param isn't provided
 delRouter.delete("/api/weather/favorites/", deleteFavoriteCity);
 delRouter.delete("/api/weather/favorites/:id", deleteFavoriteCity);
+
+delRouter.delete("/api/weather/history", headerHasUserIdCheck, deleteSearchHistory);
