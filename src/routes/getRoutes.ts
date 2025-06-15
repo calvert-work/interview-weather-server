29af6c4c56d6da4e55674df5e7dacfb62216f308
Calvert <wongcalvert-work@outlook.com>
2025-06-15 03:14:30 -0700

    Added comment to routes and made all path param optional

--- src/routes/getRoutes.ts
+++ src/routes/getRoutes.ts
@@ -12,14 +12,46 @@ import { headerHasUserIdCheck } from "../middlewares/validators/headerHasUserIdC
 
 export const getRouter = Router();
 
+/**
+ * Health check route to verify if the server is running
+ */
 getRouter.get("/healthcheck", healthcheck);
 
+/**
+ * Get current weather by city, state, and country code, city path param value can be "las vegas,nv,us" or "las vegas" or "las vegas,us" but not "las vegas,nv". 
+ * If state is provided, it must followed by the country code
+ * 
+ * Conditions:
+ * 1) Rate limited to 5 requests per min
+ * 2) Must have city value provided
+ * 3) Must have weather api key and url
+ */
 getRouter.get("/api/weather/current{/:city}", rateLimiter, hasCityValueCheck, openWeatherApiDetailsCheck, getCurrentWeatherByCity);
 
-getRouter.get("/api/weather/forecast/:city", rateLimiter, hasCityValueCheck, openWeatherApiDetailsCheck, getFiveDaysForecastByCity);
+/**
+ * Forecase 5 days weather by city, state, and country code, city path param value can be "las vegas,nv,us" or "las vegas" or "las vegas,us" but not "las vegas,nv". 
+ * If state is provided, it must followed by the country code
+ * 
+ * Conditions:
+ * 1) Rate limited to 5 requests per min
+ * 2) Must have city value provided
+ * 3) Must have weather api key and url
+ */
+getRouter.get("/api/weather/forecast{/:city}", rateLimiter, hasCityValueCheck, openWeatherApiDetailsCheck, getFiveDaysForecastByCity);
 
-getRouter.get("/api/weather/user/:email", getUser);
+/**
+ * Get "registered" user details.
+ * 
+ * Only those with a registered account can have their search history stored and city to be added to favorite
+ */
+getRouter.get("/api/weather/user{/:email}", getUser);
 
+/**
+ * Get all stored favorite cities
+ */
 getRouter.get("/api/weather/favorites", getFavoriteCities);
 
+/**
+ * Get all stored weather history
+ */
 getRouter.get("/api/weather/history", headerHasUserIdCheck, getSearchHistory);
--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -4,6 +4,12 @@ import { saveNewUser } from "../controllers/user/saveNewUser";
 
 export const postRouter = Router();
 
+/**
+ * Store one new city to favorite list
+ */
 postRouter.post("/api/weather/favorites", saveFavoriteCity);
 
+/**
+ * Register new user
+ */
 postRouter.post("/api/weather/user", saveNewUser);
