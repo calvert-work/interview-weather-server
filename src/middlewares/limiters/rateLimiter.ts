14b6777b52bf16f012ab8ca23d9d6c80da57c80e
Calvert <wongcalvert-work@outlook.com>
2025-06-15 03:18:49 -0700

    Added comments to middlewares

--- src/middlewares/limiters/rateLimiter.ts
+++ src/middlewares/limiters/rateLimiter.ts
@@ -1,6 +1,9 @@
 import rateLimit from "express-rate-limit";
 import { oneMinuteInMs } from "../../constants/constants";
 
+/**
+ * Api call is limited to 5 requests max per min
+ */
 export const rateLimiter = rateLimit({
 	windowMs: oneMinuteInMs,
 	max: 5,
